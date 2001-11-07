6c8fe2fa30782aa88f6b782fc58d2d04639cc201
fchabar <fchabar>
2001-11-07 23:16:46 +0000

    Initialisation state - The Must Unreadable By ContextbasedConstraint Type is added to the plugin and therefore to the ArgoUml design critics.

---
+++ ccl-plugin/CoCons/src/org/cocons/argo/critics/CrConstrainedReadability.java
@@ -0,0 +1,56 @@
+package org.cocons.argo.critics;
+
+import org.argouml.cognitive.critics.Critic;
+
+
+
+/**
+ * Title:        CoCons
+ * Description:  CoCons CCL Metamodel Library
+ * Copyright:    Copyright (c) 2001
+ * Company:      TU Berlin, CIS
+ * @author Fadi Chabarek
+ * @version 1.0
+ *
+ * ArgoUml Design critic to observe ContextbasedContraints with the type
+
+ * 'UnReadable' defined by the CCL.
+
+ * Therefore a scoped modelelement is criticized for reading a constrained
+
+ * modelelement, if the scoping and constraining ContextbasedConstraint has
+
+ * the type 'UnReadable'.
+
+ *
+ */
+
+public class CrConstrainedReadability extends Critic {
+
+  /**
+   * Constructs a ConstrainedReadabilityCritic.
+
+   * More work: Reading Properties from file, for changability reasons.
+
+   */
+  public CrConstrainedReadability() {
+    super();
+    this.setDescription(
+      "Observes Modelelements associated to ContextbasedContraints "
+    + "with the type 'UnReadable' defined by the Component Constraint Language.\n\n"
+    + "Therefore a scoped modelelement is criticized for reading a constrained "
+    + "modelelement, if the scoping and constraining ContextbasedConstraint has "
+    + "the type 'UnReadable'.\n\n"
+    + "Exemplary you could define in a Constraint Diagram that \n"
+    + "ALL CLASSES WHERE ´Personal Data´ EQUALS ´Yes ´ MUST BE UNREADABLE BY "
+    + "THE COMPONENT ´Data Warehouse´.\n\n"
+    + "Thus Reading a class with 'Personal Data' the DataWarehouse would raise an"
+    + "alarm by this critic");
+
+    this.setExpertEmail("info@cocons.org <info@cocons.org>");
+    this.setHeadline("Must Unreadable By");
+    this.setMoreInfoURL("http://www.cocons.org");
+
+  }
+
+}
---
+++ ccl-plugin/CoCons/src/org/cocons/argo/critics/Init.java
@@ -0,0 +1,43 @@
+package org.cocons.argo.critics;
+
+ import org.argouml.cognitive.critics.Agency;
+ import org.argouml.cognitive.critics.Critic;
+
+
+
+/**
+ * Title:        CoCons
+ * Description:  CoCons CCL Metamodel Library
+ * Copyright:    Copyright (c) 2001
+ * Company:      TU Berlin, CIS
+ * @author Fadi Chabarek
+ * @version 1.0
+ *
+ * A Plugin Initializer registering CCL-supported design critics to ArgoUml.
+ *
+ */
+
+public class Init implements Runnable {
+
+  public static Critic readabilityCritic = new CrConstrainedReadability();
+
+  /*
+   * Constructs an Initilizer
+   */
+  public Init() {
+    super();
+  }
+
+
+  /*
+   * Registers the UnReadableBy Critic to the Argo Agency singelton.
+   */
+  public void run() {
+
+    java.lang.Class modelElementCls = MModelElementImpl.class;
+
+    Agency.register(readabilityCritic, modelElementCls);
+
+
+  }
+}
