1a17285e783f74140d9ea75c39f4c2ad3733a0ab
oetker <oetker>
2001-11-13 08:43:04 +0000

    Basic Version of Business_Type Property Panel (derived from PropPanelContextbasedConstraint.java)

---
+++ ccl-plugin/CoCons/src/org/cocons/argo/ui/PropPanelBusinee_Type.java
@@ -0,0 +1,40 @@
+package org.cocons.argo.ui;
+
+import javax.swing.*;
+
+import org.tigris.gef.util.*;
+
+import org.argouml.uml.ui.foundation.core.PropPanelModelElement;
+import org.argouml.uml.ui.*;
+
+/**
+ * Title:        CoCons
+ * Description:  CoCons CCL Metamodel Library
+ * Copyright:    Copyright (c) 2001
+ * Company:      TU Berlin, CIS
+ * @author Alex Bork
+ * @version 1.0
+ */
+
+public class PropPanelBusiness_Type extends PropPanelModelElement {
+
+  protected static ImageIcon _business_TypeIcon = ResourceLoader.lookupIconResource("Business_Type");
+
+  public PropPanelBusiness_Type() {
+    super("Business_Type", _business_TypeIcon, 3);
+
+    addCaption("Name:",1,0,0);
+    addField(nameField,1,0,0);
+
+    addCaption("Stereotype:",2,0,0);
+    addField(new UMLComboBoxNavigator(this,"NavStereo",stereotypeBox),2,0,0);
+
+    addCaption("Namespace:",3,0,0);
+    addField(namespaceScroll,3,0,0);
+
+  }
+
+  protected boolean isAcceptibleBaseMetaClass(String baseClass) {
+      return baseClass.equals("Business_Type");
+  }
+}
