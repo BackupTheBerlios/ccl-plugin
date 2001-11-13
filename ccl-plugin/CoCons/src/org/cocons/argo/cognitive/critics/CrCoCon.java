219259fa3ad11522d5aa03bd0be4bcdf238f4f88
fchabar <fchabar>
2001-11-13 20:42:16 +0000

    Dokumentation vervollständigt

--- ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/CrCoCon.java
+++ ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/CrCoCon.java
@@ -5,32 +5,53 @@ import org.argouml.cognitive.Designer;
 import org.argouml.cognitive.critics.Critic;
 
 /**
- * Title:
- * Description:
+ * Title: CoCons
+ * Description: Abstract Critic subclass that defines and registers
+ * the CONSTRAINT-category of design decision that the critics can
+ * address. In contrast to the CrUML-class this class can be placed in a plugin
+ * without placing static variables in the UMLCognitiveResourceBundle to define
+ * the headline of this DesignCritic.
+ *
+ * @see org.argouml.cognitive.Designer
+ * @see org.argouml.cognitive.DecisionModel
+ * @see org.argouml.uml.cognitive.critics.UMLCognitiveResourceBundle
+ * @see org.argouml.uml.cognitive.critics.CrUML
+ *
  * Copyright:    Copyright (c) 2001
- * Company:
+ * Company: TU Berlin, CIS
  * @author Fadi Chabarek
  * @version 1.0
- * "abstract" base class. Ressource Bundle nicht über Pluginmechanismus erweiterbar.
- * Daher alle DesignCritics hiervon erben lassen.
+ *
  */
 
 public abstract class CrCoCon extends Critic {
 
+  /**
+   * Contains a Contraint-Decision
+   */
   public static final Decision decCONSTRAINT = new
      Decision("Constraint", 5);
 
+   /** Static initializer for this class. Called when the class is
+    *  loaded (which is before any subclass instances are instanciated).
+    *  The decision will now be considered by the designer, so that
+    *  extending classes just have to add the Contraint-Decision to their
+    *  supported Decisions.
+    */
   static {
     Designer d = Designer.theDesigner();
     d.startConsidering(decCONSTRAINT);
-
   }
 
+  /**
+   * Constructs a CrCoCon.
+   */
   public CrCoCon() {
-
-
   }
 
+  /**
+   * Returns true if a Contextbased Constraint is violated.
+   */
   public abstract boolean predicate(Object dm, Designer dsgr);
 
 }
