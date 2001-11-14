5195bd555e9e59933aac940bdf59ab6f75d7c8ab
fchabar <fchabar>
2001-11-14 10:52:10 +0000

    *** empty log message ***

--- ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/CrConstrainedReadability.java
+++ ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/CrConstrainedReadability.java
@@ -1,7 +1,10 @@
 package org.cocons.argo.cognitive.critics;
 
-import org.argouml.cognitive.critics.Critic;
 
+import org.argouml.cognitive.critics.Critic;
+import org.argouml.cognitive.Designer;
+
+import org.cocons.uml.ccl.MContextbasedConstraint;
 
 
 /**
@@ -13,30 +16,21 @@ import org.argouml.cognitive.critics.Critic;
  * @version 1.0
  *
  * ArgoUml Design critic to observe ContextbasedContraints with the type
-
  * 'UnReadable' defined by the CCL.
-
  * Therefore a scoped modelelement is criticized for reading a constrained
-
  * modelelement, if the scoping and constraining ContextbasedConstraint has
-
  * the type 'UnReadable'.
-
  *
  */
 
-public class CrConstrainedReadability extends Critic {
+public class CrConstrainedReadability extends CrCoCon {
 
-  /**
-   * Constructs a ConstrainedReadabilityCritic.
-
-   * More work: Reading Properties from file, for changability reasons.
-
-   */
-  public CrConstrainedReadability() {
-    super();
-    this.setDescription(
-      "Observes Modelelements associated to ContextbasedContraints "
+	////////////////////////  Decriptionelements //////////////////////////////
+	public static final String EXPERT_EMAIL = "info@cocons.org <info@cocons.org>";
+	public static final String HEADLINE = "Must Unreadable By";
+	public static final String MORE_INFO_URL = "http://www.cocons.org";
+	public static final String DESCRIPTION_EN =
+	"Observes Modelelements associated to ContextbasedContraints "
     + "with the type 'UnReadable' defined by the Component Constraint Language.\n\n"
     + "Therefore a scoped modelelement is criticized for reading a constrained "
     + "modelelement, if the scoping and constraining ContextbasedConstraint has "
@@ -45,12 +39,40 @@ public class CrConstrainedReadability extends Critic {
     + "ALL CLASSES WHERE ´Personal Data´ EQUALS ´Yes ´ MUST BE UNREADABLE BY "
     + "THE COMPONENT ´Data Warehouse´.\n\n"
     + "Thus Reading a class with 'Personal Data' the DataWarehouse would raise an"
-    + "alarm by this critic");
+    + "alarm by this critic";
 
-    this.setExpertEmail("info@cocons.org <info@cocons.org>");
-    this.setHeadline("Must Unreadable By");
-    this.setMoreInfoURL("http://www.cocons.org");
+
+  /**
+   * Constructs a ConstrainedReadabilityCritic.
+   * More work: Reading Properties from file, for changability reasons.
+   */
+  public CrConstrainedReadability() {
+    super();
+
+
+    setExpertEmail(EXPERT_EMAIL);
+    setHeadline(HEADLINE);
+    setMoreInfoURL(MORE_INFO_URL);
+    setDescription(DESCRIPTION_EN);
+
+
+    addSupportedDecision(CrCoCon.decCONSTRAINT);
 
   }
+
+  public boolean predicate(Object dm, Designer dsgr) {
+
+    MContextbasedConstraint cocon = null;
+
+    if(!(dm instanceof MContextbasedConstraint)) {
+      return NO_PROBLEM;
+    } else {
+        cocon = (MContextbasedConstraint) dm;
+    }
+    return false;
+  }
+
+
+
 
 }
