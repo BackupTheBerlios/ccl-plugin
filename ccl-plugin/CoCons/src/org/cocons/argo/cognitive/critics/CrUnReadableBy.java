0d7b6632bdd64bd6d314144bf9c10784174ee7c9
fchabar <fchabar>
2001-11-26 21:12:56 +0000

    *** empty log message ***

---
+++ ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/CrUnReadableBy.java
@@ -0,0 +1,81 @@
+package org.cocons.argo.cognitive.critics;
+
+
+import org.argouml.cognitive.critics.Critic;
+import org.argouml.cognitive.Designer;
+
+import java.util.Vector;
+
+import org.cocons.uml.ccl.MContextbasedConstraint;
+
+
+/**
+ * Title:        CoCons
+ * Description:  CoCons CCL Metamodel Library
+ * Copyright:    Copyright (c) 2001
+ * Company:      TU Berlin, CIS
+ * @author Fadi Chabarek
+ * @version 1.0
+ *
+ * ArgoUml Design critic to observe ContextbasedContraints with the type
+ * 'UnReadable' defined by the CCL.
+ * Therefore a scoped modelelement is criticized for reading a constrained
+ * modelelement, if the scoping and constraining ContextbasedConstraint has
+ * the type 'UnReadable'.
+ *
+ */
+
+public class CrUnReadableBy extends CrCoCon {
+
+	////////////////////////  Decriptionelements //////////////////////////////
+	public static final String EXPERT_EMAIL = "info@cocons.org <info@cocons.org>";
+	public static final String HEADLINE = "Must Unreadable By";
+	public static final String MORE_INFO_URL = "http://www.cocons.org";
+	public static final String DESCRIPTION_EN =
+	"Observes Modelelements associated to ContextbasedContraints "
+    + "with the type 'UnReadable' defined by the Component Constraint Language.\n\n"
+    + "Therefore a scoped modelelement is criticized for reading a constrained "
+    + "modelelement, if the scoping and constraining ContextbasedConstraint has "
+    + "the type 'UnReadable'.\n\n"
+    + "Exemplary you could define in a Constraint Diagram that \n"
+    + "ALL CLASSES WHERE ´Personal Data´ EQUALS ´Yes ´ MUST BE UNREADABLE BY "
+    + "THE COMPONENT ´Data Warehouse´.\n\n"
+    + "Thus Reading a class with 'Personal Data' the DataWarehouse would raise an"
+    + "alarm by this critic";
+
+
+  /**
+   * Constructs a ConstrainedReadabilityCritic.
+   * More work: Reading Properties from file, for changability reasons.
+   */
+  public CrUnReadableBy() {
+    super();
+
+
+    setExpertEmail(EXPERT_EMAIL);
+    setHeadline(HEADLINE);
+    setMoreInfoURL(MORE_INFO_URL);
+    setDescription(DESCRIPTION_EN);
+
+
+    addSupportedDecision(CrCoCon.decCONSTRAINT);
+
+  }
+
+  public boolean predicate(Object dm, Designer dsgr) {
+
+    MContextbasedConstraint cocon = null;
+
+    if(!(dm instanceof MContextbasedConstraint)) {
+      return NO_PROBLEM;
+    } else {
+        cocon = (MContextbasedConstraint) dm;
+
+    }
+    return false;
+  }
+
+
+
+
+}
---
+++ ccl-plugin/CoCons/src/org/cocons/argo/cognitive/critics/test/TestCrUnReadableBy.java
@@ -0,0 +1,62 @@
+package org.cocons.argo.cognitive.critics.test;
+
+import junit.framework.TestCase;
+
+import org.argouml.kernel.Project;
+import org.argouml.ui.ProjectBrowser;
+
+import org.cocons.uml.ccl.*;
+
+
+/**
+ * Title: TestCrUnReadabableBy.java
+ * Description: This test case tests the class CrConstrainedReadability.
+ * Copyright:    Copyright (c) 2001
+ * Company: TU-Berlin, CIS
+ * @author Philipp Schumacher, Fadi Chabarek
+ * @version 1.0
+ */
+
+public class TestCrUnReadableBy extends TestCase {
+
+  MContextbasedConstraint cocon;
+  MContextCondition scopeSet;
+  MContextCondition constrainedSet;
+  Project project;
+
+  public TestCrUnReadableBy(String name) {
+    super(name);
+  }
+
+  public static void main(String[] args) {
+    junit.swingui.TestRunner.run(TestCrUnReadableBy.class);
+  }
+
+  public void setUp() {
+
+  /*
+    cocon = new MContextbasedConstraintImpl();
+    scopeSet = new MContextConditionImpl();
+    constrainedSet = new MContextConditionImpl();
+
+    scopeSet.addContextbasedConstraint(cocon);
+    constrainedSet.addContextbasedConstraint(cocon);
+
+    cocon.setScopeSet(scopeSet);
+    cocon.setConstrainedSet(constrainedSet);
+
+    project = ProjectBrowser.TheInstance.getProject();
+*/
+
+
+  }
+
+  public void tearDown() {
+
+
+  }
+
+  public void testPredicate() {
+    assertTrue("tautologisch", true);
+  }
+}
