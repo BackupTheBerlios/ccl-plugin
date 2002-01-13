757b5c4169aa8d8317ce811b084fa32863b6b288
jgusulde <jgusulde>
2002-01-13 10:08:20 +0000

    *** empty log message ***

---
+++ ccl-plugin/CoCons/src/org/cocons/argo/diagram/atomic_invocation_path/AtomicInvocationPathDiagramGraphModel.java
@@ -0,0 +1,10 @@
+package org.cocons.argo.diagram.atomic_invocation_path;
+
+import org.argouml.uml.diagram.sequence.SequenceDiagramGraphModel;
+
+public class AtomicInvocationPathDiagramGraphModel extends SequenceDiagramGraphModel {
+
+  public AtomicInvocationPathDiagramGraphModel() {
+    super();
+  }
+}
