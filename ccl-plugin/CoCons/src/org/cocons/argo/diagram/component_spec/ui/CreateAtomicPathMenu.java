5df881c211711c275c668d9e824406e02d261d18
jgusulde <jgusulde>
2002-01-15 16:47:40 +0000

    component_spec/ui/CreateAtomicPathMenu.java

--- ccl-plugin/CoCons/src/org/cocons/argo/diagram/component_spec/ui/CreateAtomicPathMenu.java
+++ ccl-plugin/CoCons/src/org/cocons/argo/diagram/component_spec/ui/CreateAtomicPathMenu.java
@@ -11,16 +11,20 @@ package org.cocons.argo.diagram.component_spec.ui;
 import java.util.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.beans.*;
+
 import ru.novosoft.uml.foundation.core.*;
 import ru.novosoft.uml.foundation.extension_mechanisms.*;
+import ru.novosoft.uml.behavior.common_behavior.*;
 import org.tigris.gef.base.*;
 import org.tigris.gef.graph.*;
 import org.argouml.ui.*;
 import org.argouml.kernel.*;
+import org.argouml.uml.diagram.ui.ActionAddExistingNode;
+import org.argouml.uml.ui.*;
+
 import org.cocons.argo.diagram.ui.*;
 import org.cocons.argo.diagram.atomic_invocation_path.ui.*;
-import org.argouml.uml.ui.*;
-import java.beans.*;
 
 public class CreateAtomicPathMenu {
 
@@ -58,18 +62,27 @@ public class CreateAtomicPathMenu {
           Project p = pb.getProject();
           try {
             Object target = ProjectBrowser.TheInstance.getDetailsTarget();
-            if(target instanceof MInterface){
-              MInterface mi = (MInterface) target;
-              MNamespace ns = mi.getNamespace();
-              System.out.println("-----------" + target +"------"+ ns);
+            if(target instanceof MClassifier){
+              MClassifier mc = (MClassifier) target;
+              MNamespace ns = mc.getNamespace();
               ArgoDiagram d = new CCLAtomicInvocationPathDiagram(ns);
               p.addMember(d);
               ProjectBrowser.TheInstance.getNavPane().addToHistory(d);
               ProjectBrowser.TheInstance.setTarget(d);
               d.setName("Atomic Invocation Path Diagram of Operation '" + oper.getName()
               +"' in Interface '" + inter.getName() +"'");
-              //Action actionObject = new CmdCreateNode(MObjectImpl.class, "Object");
-              //actionObject.actionPerformed(new ActionEvent());
+
+              //ActionAddExistingNode addNode = new ActionAddExistingNode("",mi);
+              //addNode.actionPerformed(null);
+              Action actionObject = new CmdCreateNode(MObjectImpl.class, "Object");
+              actionObject.actionPerformed(null);
+              Object o = ProjectBrowser.TheInstance.getDetailsTarget();
+              if(!(o instanceof MObject)){System.out.println("--- CreateAtomicPathMenu.CreateAtomicPathAction.actionPerformed(): Object not found " + o );return;}
+              MObject mo = (MObject) o;
+              mo.setName("System");
+              Action actionObject2 = new CmdCreateNode(MObjectImpl.class, "Object");
+              actionObject2.actionPerformed(null);
+
             }
           }
           catch (PropertyVetoException pve) { }
