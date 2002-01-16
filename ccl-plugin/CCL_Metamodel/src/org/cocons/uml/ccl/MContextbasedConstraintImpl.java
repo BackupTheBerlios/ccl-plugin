caa0b2fa91de027f987c5f6a2f943ec083fcfafe
gingyong <gingyong>
2002-01-16 20:41:23 +0000

    müsste jetzt klappen

--- ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/MContextbasedConstraintImpl.java
+++ ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/MContextbasedConstraintImpl.java
@@ -39,13 +39,13 @@ public class MContextbasedConstraintImpl
 	 * The Vector that stores the Elements in the TargetSet, but only those
 	 * that are bind indirect via a context condition.
 	 */
-	private Vector _targetElements = new Vector();
+	private Vector _targetSetIndirectElements = new Vector();
 
 	/**
 	 * The Vector that stores the Elements in the ScopeSet, but only those that
 	 * are bind indirect via a context condition.
 	 */
-	private Vector _scopeElements = new Vector();
+	private Vector _scopeSetIndirectElements = new Vector();
 
 	/**
 	 * This Vector contains all the direct elements that are in the TargetSet,
@@ -54,7 +54,7 @@ public class MContextbasedConstraintImpl
 	 * references of the scope and taret set that are stored together in the
 	 * parent Constraint.
 	 */
-	private Vector _targetSetDirectReferences = null;
+	private Vector _targetSetDirectElements = null;
 
 	/**
 	 * This Vector contains all the direct elements that are in the ScopeSet,
@@ -63,7 +63,7 @@ public class MContextbasedConstraintImpl
 	 * references of the scope and taret set that are stored together in the
 	 * parent Constraint.
 	 */
-	private Vector _scopeSetDirectReferences = null;
+	private Vector _scopeSetDirectElements = null;
 
 	/**
 	 * Constructor that creates a new Contextbased Constraint.
@@ -92,7 +92,25 @@ public class MContextbasedConstraintImpl
 	 *         return null if this CoCon hasn't been initialized yet.
 	 */
 	public Vector getScopedElements() {
-          return null;
+          if ((_scopeSetIndirectElements!=null) && (_scopeSetDirectElements!=null)) {
+            // return direct and indirect elements
+            Vector allElements = new Vector();
+            for (int i=0; i<_scopeSetDirectElements.size(); i++) {
+              allElements.add(_scopeSetDirectElements.elementAt(i));
+            }
+            for (int i=0; i<_scopeSetIndirectElements.size(); i++) {
+              allElements.add(_scopeSetIndirectElements.elementAt(i));
+            }
+            return allElements;
+          } else if (_scopeSetIndirectElements!=null) {
+            //return only the indirect elements
+            return _scopeSetIndirectElements;
+          } else if (_scopeSetDirectElements!=null) {
+            //return only direct elements
+            return _scopeSetDirectElements;
+          } else {
+            return null;
+          }
 	}
 
 	/**
@@ -103,72 +121,6 @@ public class MContextbasedConstraintImpl
 	public String getCoConType() {
 		return _coconType;
 	}
-
-	/**
-	 * Updates the TargetSet and ScopeSet
-	 */
-	private void updateSets() {
-          /*      //_coconType = _xmlCCL.getType();
-
-		ModelIterator ite = new ModelIterator();
-		Vector modelElements = ite.getAllModelElements();
-
-		// parse into Syntax tree
-		XMLDataToContextConditionParser parser = new XMLDataToContextConditionParserImpl();
-
-		// evaluate TargetSet
-		if (parsedCoCon.getTargetSet().getDirectReferences() != null) {
-			// direct references
-			_targetSetDirectReferences = new Vector();
-			Collection directReferences = parsedCoCon.getTargetSet().getDirectReferences();
-			while (directReferences.iterator().hasNext()) {
-				String directName = directReferences.iterator().next().toString();
-				MModelElement directElement = ite.getModelElementClass(directName);
-				if (directElement != null) {
-					_targetSetDirectReferences.addElement(directElement);
-					addConstrainedElement(directElement);
-				}
-			}
-		} else {
-			// indirect references
-			_targetSetDirectReferences = null;
-			_targetSet = parser.generateContextCondition(parsedCoCon.getTargetSet());
-			for (int i = 0; i < modelElements.size(); i++) {
-				MModelElement element = (MModelElement) modelElements.elementAt(i);
-				if (_targetSet.isCompliedWith(element)) {
-					_targetElements.addElement(element);
-				}
-			}
-		}
-
-		// evaluate ScopeSet
-		if (parsedCoCon.getScopeSet().getDirectReferences() != null) {
-			// direct references
-			_scopeSetDirectReferences = new Vector();
-			Collection directReferences = parsedCoCon.getScopeSet().getDirectReferences();
-			while (directReferences.iterator().hasNext()) {
-				String directName = directReferences.iterator().next().toString();
-				MModelElement directElement = ite.getModelElementClass(directName);
-				if (directElement != null) {
-					_scopeSetDirectReferences.addElement(directElement);
-					addConstrainedElement(directElement);
-				}
-			}
-		} else {
-			// indirect references
-			_scopeSetDirectReferences = null;
-			_scopeSet = parser.generateContextCondition(parsedCoCon.getScopeSet());
-			for (int i = 0; i < modelElements.size(); i++) {
-				MModelElement element = (MModelElement) modelElements.elementAt(i);
-				if (_scopeSet.isCompliedWith(element)) {
-					_scopeElements.addElement(element);
-				}
-			}
-		}
-          */
-	}
-
-
 
 	/**
 	 * Returns the scope set context condition.
@@ -196,42 +148,121 @@ public class MContextbasedConstraintImpl
 	}
 
 	/**
-	 * Sets the context condition for the scope set.
+	 * Sets the context condition for the scope set. Calling
+         * this method with parameter null will reset the scope set.
          *
 	 * @param condition the ContextCondition
 	 */
 	public void setScopeSetContextCondition(ContextCondition condition) {
-		_scopeSet = condition;
+	  if (condition==null) {
+            _scopeSetIndirectElements = null;
+            _scopeSet = null;
+
+          } else {
+            _scopeSet = condition;
+            // and get all model elements that belong to this condition
+            ModelIterator ite = new ModelIterator();
+	    Vector modelElements = ite.getAllModelElements();
+            for (int i = 0; i < modelElements.size(); i++) {
+	      MModelElement element = (MModelElement) modelElements.elementAt(i);
+              if (_scopeSet.isCompliedWith(element)) {
+                _scopeSetIndirectElements.addElement(element);
+              }
+            }
+          }
 	}
 
 	/**
-	 * Sets the context condition for the target set.
+	 * Sets the context condition for the target set. Calling
+         * this method with parameter null will reset the target set.
 	 *
 	 * @param condition the ContextCondition
          */
 	public void setTargetSetContextCondition(ContextCondition condition) {
-		_targetSet = condition;
+	  if (condition==null) {
+            _targetSetIndirectElements = null;
+            _targetSet = null;
+
+          } else {
+            _targetSet = condition;
+            // and get all model elements that belong to this condition
+            ModelIterator ite = new ModelIterator();
+	    Vector modelElements = ite.getAllModelElements();
+            for (int i = 0; i < modelElements.size(); i++) {
+	      MModelElement element = (MModelElement) modelElements.elementAt(i);
+              if (_targetSet.isCompliedWith(element)) {
+                _targetSetIndirectElements.addElement(element);
+              }
+            }
+          }
 	}
 
         /**
-         * Sets the direct associated elements for the target set.
+         * Sets the direct associated elements for the target set. Calling
+         * this method with parameter null will reset the direct scope set.
          *
          * @param directElements the vector that contains the direct
          *        elements for the target set. Each element is a String,
          *        the name of the direct associated element.
          */
         public void setTargetSetDirectElements(Vector directElements) {
+          if (directElements==null) {
+            if (_targetSetDirectElements!=null) {
+              // delete the direct elements in the target set from the constraint
+              for (int i=0; i<_targetSetDirectElements.size(); i++) {
+                MModelElement element = (MModelElement)_targetSetDirectElements.elementAt(i);
+                deleteDirectModelElementFromConstraint(element);
+              }
+            }
+            _targetSetDirectElements = null;
+            return;
 
+          } else {
+            _targetSetDirectElements = new Vector();
+            ModelIterator ite = new ModelIterator();
+            for (int i=0; i<directElements.size(); i++) {
+              String directName = directElements.elementAt(i).toString();
+              MModelElement directElement = ite.getModelElementClass(directName);
+              if (directElement != null) {
+		_targetSetDirectElements.addElement(directElement);
+		addConstrainedElement(directElement);
+              }
+            }
+          }
         }
 
         /**
-         * Sets the direct associated elements for the scope set.
+         * Sets the direct associated elements for the scope set. Calling
+         * this method with parameter null will reset the direct scope set.
          *
          * @param directElements the vector that contains the direct
          *        elements for the scope set. Each element is a String,
          *        the name of the direct associated element.
          */
         public void setScopeSetDirectElements(Vector directElements) {
+          if (directElements==null) {
+            if (_scopeSetDirectElements!=null) {
+              // delete the direct elements in the scope set from the constraint
+              for (int i=0; i<_scopeSetDirectElements.size(); i++) {
+                MModelElement element = (MModelElement)_scopeSetDirectElements.elementAt(i);
+                deleteDirectModelElementFromConstraint(element);
+              }
+            }
+            _scopeSetDirectElements = null;
+            return;
+
+          } else {
+            _scopeSetDirectElements = new Vector();
+            ModelIterator ite = new ModelIterator();
+            for (int i=0; i<directElements.size(); i++) {
+              String directName = directElements.elementAt(i).toString();
+              MModelElement directElement = ite.getModelElementClass(directName);
+              if (directElement != null) {
+		_scopeSetDirectElements.addElement(directElement);
+		addConstrainedElement(directElement);
+              }
+            }
+          }
         }
 
         /**
@@ -241,7 +272,11 @@ public class MContextbasedConstraintImpl
          *         if the CoCon hasn't been initialized yet.
          */
         public String toString() {
-          return null;
+          if ((getTargetSetCCLString()!=null)&&(getScopeSetCCLString()!=null)&&(getCoConType()!=null)) {
+            return getTargetSetCCLString() + getCoConType() + getScopeSetCCLString();
+          } else {
+            return null;
+          }
         }
 
         /**
@@ -250,7 +285,7 @@ public class MContextbasedConstraintImpl
          * @return the CCL String that defines the target set.
          */
         public String getTargetSetCCLString() {
-          return null;
+          return _targetSet.toString();
         }
 
         /**
@@ -259,6 +294,22 @@ public class MContextbasedConstraintImpl
          * @return the CCL String that defines the scope set.
          */
         public String getScopeSetCCLString() {
-          return null;
+          return _scopeSet.toString();
+        }
+
+        /**
+         * Deletes a direct associated element from the parent Constraint.
+         *
+         * @param modelElement the model element to delete.
+         */
+        private void deleteDirectModelElementFromConstraint(MModelElement modelElement) {
+          List list = getConstrainedElements();
+          while (list.iterator().hasNext()) {
+            MModelElement element = (MModelElement)list.iterator().next();
+            if (element.equals(modelElement)) {
+              list.remove(element);
+              return;
+            }
+          }
         }
 }
