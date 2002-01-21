8286868685ddbd1bb522391dbd880b93b6100cad
rolfexner <rolfexner>
2002-01-21 16:10:21 +0000

    new Methods: buildConditionTree(Vector compoString) updateModel()

--- ccl-plugin/CoCons/src/org/cocons/argo/diagram/constraint/ui/FigContextbasedConstraint.java
+++ ccl-plugin/CoCons/src/org/cocons/argo/diagram/constraint/ui/FigContextbasedConstraint.java
@@ -13,6 +13,9 @@ import org.argouml.uml.diagram.ui.*;
 
 import ru.novosoft.uml.foundation.core.*;
 import ru.novosoft.uml.foundation.extension_mechanisms.*;
+import ru.novosoft.uml.foundation.data_types.MMultiplicity;
+import org.cocons.uml.ccl.comparators.*;
+import org.cocons.uml.ccl.*;
 import org.cocons.uml.ccl.*;
 
 
@@ -44,11 +47,11 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
 
     // add other Figs here aes needed
     protected FigGroup _scopeVec;
-    protected FigGroup _constraintVec;
+    protected FigGroup _targetVec;
 
     protected FigRect _scopePort;
-    protected FigRect _constraintPort;
-    protected FigText _constraintText;
+    protected FigRect _targetPort;
+    protected FigText _targetText;
     protected FigText _scopeText;
     public MElementResidence resident = new MElementResidenceImpl();
 
@@ -101,22 +104,22 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
         _arrowhead.addPoint(120,25);
 
         //construction of the Target-(Constraint-)Set
-        _constraintPort = new FigRect(0,0,80,40, Color.gray, Color.white);
-        _constraintPort.setLineWidth(1);
-        _constraintPort.setFilled(true);
-        _constraintPort.translate(120,0);
+        _targetPort = new FigRect(0,0,80,40, Color.gray, Color.white);
+        _targetPort.setLineWidth(1);
+        _targetPort.setFilled(true);
+        _targetPort.translate(120,0);
 
-        _constraintVec = new FigGroup();
-        _constraintVec.setFilled(true);
-        _constraintVec.setLineWidth(1);
-        _constraintVec.addFig(_constraintPort);
-        //_constraintVec.translate(120,0);
-        _constraintText = new FigText(5,5,16,15, Color.black,"TimesRoman", 10);
-        _constraintText.setText("Enter Constraint Set");
-        _constraintText.setFilled(false);
-        _constraintText.setEditable(true);
-        _constraintText.setLineWidth(0);
-        _constraintText.setWidth(85);
+        _targetVec = new FigGroup();
+        _targetVec.setFilled(true);
+        _targetVec.setLineWidth(1);
+        _targetVec.addFig(_targetPort);
+        //_targetVec.translate(120,0);
+        _targetText = new FigText(5,5,16,15, Color.black,"TimesRoman", 10);
+        _targetText.setText("Enter target Set");
+        _targetText.setFilled(false);
+        _targetText.setEditable(true);
+        _targetText.setLineWidth(0);
+        _targetText.setWidth(85);
 
         //construction of the text-field for the cocon-type
         _stereo.setBounds(60, 7, 45, 15);
@@ -144,15 +147,14 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
         addFig(_arrowhead);
         addFig(_triangle);
         addFig(_letter_i);
-        addFig(_constraintPort);
+        addFig(_targetPort);
         addFig(_name);
         addFig(_stereo);
-        addFig(_constraintText);
+        addFig(_targetText);
         addFig(_scopeText);
-        //addFig(_constraintVec);
+        //addFig(_targetVec);
         modelChanged();
     }
-
 
     public FigContextbasedConstraint(GraphModel gm, Object node) {
         this();
@@ -170,10 +172,10 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
         figClone._arrowhead = (FigPoly) v.elementAt(2);
         figClone._triangle = (FigPoly) v.elementAt(3);
         figClone._letter_i = (FigText) v.elementAt(4);
-        figClone._constraintPort = (FigRect) v.elementAt(5);
+        figClone._targetPort = (FigRect) v.elementAt(5);
         figClone._name = (FigText) v.elementAt(6);
         figClone._stereo = (FigText) v.elementAt(7);
-        figClone._constraintText = (FigText) v.elementAt(8);
+        figClone._targetText = (FigText) v.elementAt(8);
         figClone._scopeText = (FigText) v.elementAt(9);
 
         return figClone;
@@ -271,23 +273,129 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
     }
   }
 
+
+  protected void updateModel() {
+    MContextbasedConstraint me = (MContextbasedConstraint) getOwner();
+    if (me == null) return;
+
+    me.setCoConType("hier muﬂ noch die Checkbox ausgelesen werden...");
+
+    /*
+    * Read the Target-Set Text-Box and commit it to the Owner
+    */
+
+    String targetString = _targetText.getText();
+    Vector compoString = new Vector();
+    StringTokenizer st = new StringTokenizer(targetString);
+    Vector conditionStrings = new Vector();
+    Vector indirectAssoziations = new Vector();
+    Vector directAssoziations = new Vector();
+    int act = 0;
+
+    // fill compoString with the plain text in the targetSet
+    while (st.hasMoreTokens()) {
+	compoString.addElement(st.nextToken());
+    }
+
+    // fill conditionStrings with the Vectors with the conditions
+
+    for (int k = 0; k < compoString.size(); k++){
+      if (compoString.get(k).toString().equals("or") || compoString.get(k).toString().equals("OR")){
+        act = k;
+        Vector helpString = new Vector();
+        for (int i = act; i < k; i++){
+          helpString.addElement(compoString.get(i));
+        }
+        conditionStrings.addElement(helpString);
+      }
+    }
+
+    //compute the conditions and set the Target-Set
+    for (int i = 0; i < conditionStrings.size(); i++){
+      Vector actVect = (Vector) conditionStrings.get(i);
+
+      //if indirect assoziation
+      if (actVect.get(0).toString().equals("all") || actVect.get(0).toString().equals("ALL")){
+        ContextCondition indirectCondition = buildConditionTree(actVect);
+        //fill the Vector conditionTrees with all the indirect Assoziation Trees
+        indirectAssoziations.addElement(indirectCondition);
+      }
+      //if direct assoziation
+      else{
+        //me.setTargetSetDirectElements(actVect);
+        for (int j = 0; j < actVect.size(); j++){
+          //fill the Vector directAssoziations with all the direct Elements
+          directAssoziations.addElement( actVect.get(j));
+        }
+      }
+    }
+    //Now all Assoziations lay in the Vectors directAssoziations and indirectAssoziations
+
+    //The direct Assoziations are ready for commit:
+    me.setTargetSetDirectElements(directAssoziations);
+    //The indirect Assoziations must be a Tree of the type ContextCondition:
+
+    //... to Do: Built Tree out of the conditions and commit
+
+
+   /*
+    * Read the Scope-Set Text-Box and commit it to the Owner
+    */
+
+    //Needs more work
+
+    //falls keine indirekte Assoziation besteht, wird als Parameter "null" ¸bergeben
+  }
+
+
+  protected ContextCondition buildConditionTree(Vector compoString){
+
+    ContextConditionImpl target = new ContextConditionImpl();
+    ComparatorFactoryImpl comparatorFactory = new ComparatorFactoryImpl();
+
+    MMultiplicity range = new MMultiplicity( (compoString.get(0)).toString());
+    String baseClass = (compoString.get(1)).toString();
+
+    ComparisonImpl b = new ComparisonImpl();
+    String condition = "";
+    String value = "";
+    for (int i = 3; i < compoString.size(); i++) {
+      if (((compoString.get(i)).toString()).equals("=") ||((compoString.get(i)).toString()).equals("EQUALS")) {
+        b.setComparator(comparatorFactory.produceComparatorWithType(ComparatorFactory.EQUAL));
+        for(int j = 3; j < i; j++){
+            condition = condition + compoString.get(j) + " ";
+        }
+        for(int h = i+1; h < compoString.size(); h++){
+          value =value + compoString.get(h) + " ";
+        }
+      }
+    }
+
+    target.setRange(range);
+    target.setBaseClass(baseClass);
+    target.setComparison(b);
+    return target;
+  }
+
+
+
   /*
   *CalculateSetBoxes()
   *function to give the boxes of Scope-Set and Target(Constraint)-Set the right size
   */
   protected void calculateSetBoxes(){
 
-    Dimension constraintDim = _constraintText.getSize();
+    Dimension targetDim = _targetText.getSize();
     Dimension scopeDim = _scopeText.getSize();
 
-    int newConstraintWidth = Math.max((int)constraintDim.getWidth(), 40);
-    int newConstraintHeight = Math.max((int)constraintDim.getHeight(), 40);
+    int newtargetWidth = Math.max((int)targetDim.getWidth(), 40);
+    int newtargetHeight = Math.max((int)targetDim.getHeight(), 40);
     int newScopeWidth = Math.max((int)scopeDim.getWidth(), 40);
     int newScopeHeight = Math.max((int)scopeDim.getHeight(), 40);
-    newConstraintHeight = Math.max(newConstraintHeight, newScopeHeight);
-    newConstraintWidth = Math.max(newScopeWidth, newConstraintWidth);
-    Dimension newDimension = new Dimension (newConstraintWidth, newConstraintHeight);
-    _constraintPort.setSize(newDimension);
+    newtargetHeight = Math.max(newtargetHeight, newScopeHeight);
+    newtargetWidth = Math.max(newScopeWidth, newtargetWidth);
+    Dimension newDimension = new Dimension (newtargetWidth, newtargetHeight);
+    _targetPort.setSize(newDimension);
     _scopePort.setSize(newDimension);
 
   }
@@ -296,20 +404,20 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
   protected void modelChanged() {
 
     super.modelChanged();
-    //calculate the size of ConstraintSetBox and ScopeSetBox
+    //calculate the size of targetSetBox and ScopeSetBox
     calculateSetBoxes();
     calcBounds();
     Rectangle rect = getBounds();
 
     // calculate new height
-    int new_height = _constraintPort.getHeight();
+    int new_height = _targetPort.getHeight();
     // calculate new width
     int new_width = 41; // width of constant figs
     if (_stereo.isDisplayed() )
       new_width = new_width+ Math.max(_name.getWidth(),_stereo.getWidth());
     else
       new_width = new_width+_name.getWidth();
-    new_width = new_width + (2 * _constraintPort.getWidth());
+    new_width = new_width + (2 * _targetPort.getWidth());
 
     //figure will be resized and rearanged
     setBounds(rect.x-(new_width-rect.width)/2, rect.y-(new_height-rect.height), new_width, new_height);
@@ -320,7 +428,7 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
     calcBounds();
     Rectangle oldBounds = getBounds();
     // calculate new height
-    h = _constraintPort.getHeight();
+    h = _targetPort.getHeight();
 
     // calculate new width
     int new_width = 41; // width of constant figs
@@ -329,22 +437,22 @@ public class FigContextbasedConstraint extends FigNodeModelElement {
     else
       new_width = new_width+_name.getWidth();
 
-      w = new_width + (2 * _constraintPort.getWidth());
+      w = new_width + (2 * _targetPort.getWidth());
 
-      int textBoxWidth = _constraintPort.getWidth(); //By "CalculateTextBoxes" it is sure that size of _constraintPort and _scopePort is equal
+      int textBoxWidth = _targetPort.getWidth(); //By "CalculateTextBoxes" it is sure that size of _targetPort and _scopePort is equal
 
       int coconTypeWidth = _name.getWidth();
 
-      _constraintPort.setLocation(x, y);
-      _constraintText.setLocation(x, y);
-      _triangle.setLocation(x+textBoxWidth, y + _constraintPort.getHalfHeight()-8);
-      _letter_i.setLocation(x+textBoxWidth, y + _constraintPort.getHalfHeight()-8);
-      _name.setLocation(x+ w / 2 - _name.getWidth() / 2, y + _constraintPort.getHalfHeight() + 5);
+      _targetPort.setLocation(x, y);
+      _targetText.setLocation(x, y);
+      _triangle.setLocation(x+textBoxWidth, y + _targetPort.getHalfHeight()-8);
+      _letter_i.setLocation(x+textBoxWidth, y + _targetPort.getHalfHeight()-8);
+      _name.setLocation(x+ w / 2 - _name.getWidth() / 2, y + _targetPort.getHalfHeight() + 5);
       if (_stereo.isDisplayed() )
-        _stereo.setLocation(x+ w / 2 - _stereo.getWidth()/2, y + _constraintPort.getHalfHeight() - 18);
-      _arrow.setPoints(0, x + textBoxWidth + 16, y + _constraintPort.getHalfHeight() );
-      _arrow.setPoints(1, x + w - textBoxWidth - 10, y + _constraintPort.getHalfHeight() );
-      _arrowhead.setLocation(x + w - textBoxWidth - 10,  y + _constraintPort.getHalfHeight() - 3);
+        _stereo.setLocation(x+ w / 2 - _stereo.getWidth()/2, y + _targetPort.getHalfHeight() - 18);
+      _arrow.setPoints(0, x + textBoxWidth + 16, y + _targetPort.getHalfHeight() );
+      _arrow.setPoints(1, x + w - textBoxWidth - 10, y + _targetPort.getHalfHeight() );
+      _arrowhead.setLocation(x + w - textBoxWidth - 10,  y + _targetPort.getHalfHeight() - 3);
       _scopePort.setLocation(x + w -textBoxWidth, y);
       _scopeText.setLocation(x + w -textBoxWidth, y);
 
