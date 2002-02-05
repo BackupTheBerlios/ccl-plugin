e4f5234309c42cc93325dc24e0e1161fddc68afd
klischat <klischat>
2002-02-05 12:03:20 +0000

    Type-Im- und -Exports

--- ccl-plugin/CoCons/src/org/cocons/argo/diagram/cml/ComponentSpecWriter.java
+++ ccl-plugin/CoCons/src/org/cocons/argo/diagram/cml/ComponentSpecWriter.java
@@ -7,6 +7,8 @@ import ru.novosoft.uml.foundation.extension_mechanisms.*;
 import ru.novosoft.uml.foundation.data_types.*;
 import ru.novosoft.uml.model_management.*;
 import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
 import org.exolab.castor.xml.*;
 
 //use the XML pretty-printing classes from Apache's SAX implementation.
@@ -117,69 +119,125 @@ public class ComponentSpecWriter
        p.addValue("42");
    }
 
+
    protected void buildExports( ComponentSpec cs )
    {
-      /* TODO: Find all MInterfaces exported by _component,
-       * let the (not yet existing) CMLFacadeBuilder build
-       * an according Facade, and include it.
-       */
+       /* TODO: Find all MInterfaces exported by _component,
+        * let the (not yet existing) CMLFacadeBuilder build
+        * an according Facade, and include it.
+        */
 
-      // example stuff - active because they're required fields
-      ExportSection es = new ExportSection();
-      cs.setExportSection(es);
+       // example stuff - active because they're required fields
+       ExportSection es = new ExportSection();
+       cs.setExportSection(es);
 
-      ExportSectionChoice esc = new ExportSectionChoice();
-      es.setExportSectionChoice(esc);
+       ExportSectionChoice esc = new ExportSectionChoice();
+       es.setExportSectionChoice(esc);
 
-      for (Iterator it=_component.getAssociationEnds().iterator();
-           it.hasNext();) {
-        MAssociationEnd end = (MAssociationEnd)it.next();
-        MClassifier mc = (MClassifier)end.getOppositeEnd().getType();
-        if (mc instanceof MInterface && mc.getStereotype().getName().equals("interface spec")) {
-           Facade f = new Facade();
-           esc.addFacade(f);
-           //es.addFacade(f);
-           f.setFacadeName(mc.getName());
-           BusinessExport be = new BusinessExport();
-           f.addBusinessExport(be);
-           MInterface mint = (MInterface)mc;
-           for (Iterator it2 = mint.getFeatures().iterator(); it2.hasNext(); ) {
-            Object o = it2.next();
-            if (o instanceof MOperation) {
-              MOperation op = (MOperation)o;
-              be.addMethod(operationToMethod(op));
-            }
+       for (Iterator it=_component.getAssociationEnds().iterator();
+            it.hasNext();) {
+           MAssociationEnd end = (MAssociationEnd)it.next();
+           MClassifier mc = (MClassifier)end.getOppositeEnd().getType();
+           if (mc instanceof MInterface && mc.getStereotype().getName().equals("interface spec")) {
+               Facade f = new Facade();
+               esc.addFacade(f);
+               //es.addFacade(f);
+               f.setFacadeName(mc.getName());
+               BusinessExport be = new BusinessExport();
+               f.addBusinessExport(be);
+               MInterface mint = (MInterface)mc;
+               for (Iterator it2 = mint.getFeatures().iterator(); it2.hasNext(); ) {
+                   Object o = it2.next();
+                   if (o instanceof MOperation) {
+                       MOperation op = (MOperation)o;
+                       be.addMethod(operationToMethod(op));
+                   }
+               }
+
+               //find exported types for this facade
+
+               TypeExport typeExport = null;
+               for (Iterator it2=mint.getAssociationEnds().iterator();
+                    it2.hasNext();) {
+                   MClassifier mc2 = (MClassifier)((MAssociationEnd)it2.next()).getOppositeEnd().getType();
+                   if (mc2 instanceof MClass && mc2.getStereotype().getName().equals("info type")) {
+                       MClass mtype = (MClass)mc2;
+
+                       if (typeExport == null) {
+                           typeExport = new TypeExport();
+                           f.setTypeExport(typeExport);
+                       }
+
+                       typeExport.addType(mTypeToCMLType(mtype));
+                   }
+               }
            }
-
-        }
-      }
+       }
    }
 
   protected void buildImports( ComponentSpec cs ){
-    ImportSection is = new ImportSection();
-    cs.setImportSection(is);
-    BusinessImport bi = new BusinessImport();
-    is.setBusinessImport(bi);
-    for (Iterator it=_component.getClientDependencies().iterator();
+      ImportSection is = null;
+      BusinessImport bi = null;
+
+      Set importedTypes = null;
+      for (Iterator it=_component.getClientDependencies().iterator();
            it.hasNext();){
-      MDependency md = (MDependency) it.next();
+          MDependency md = (MDependency) it.next();
 
-      for(Iterator it2 = md.getSuppliers().iterator(); it2.hasNext();){
-        MModelElement mc = (MModelElement)it2.next();
+          for(Iterator it2 = md.getSuppliers().iterator(); it2.hasNext();){
+              MModelElement mc = (MModelElement)it2.next();
 
-        if (mc instanceof MInterface && mc.getStereotype().getName().equals("interface spec")) {
-          MInterface mint = (MInterface)mc;
-          for (Iterator it3 = mint.getFeatures().iterator(); it3.hasNext(); ) {
-            Object o = it3.next();
-            if (o instanceof MOperation) {
-              MOperation op = (MOperation)o;
-              bi.addMethod(operationToMethod(op));
-            }
+              if (mc instanceof MInterface && mc.getStereotype().getName().equals("interface spec")) {
+                  if (is == null) {
+                      is = new ImportSection();
+                      cs.setImportSection(is);
+                      bi = new BusinessImport();
+                      is.setBusinessImport(bi);
+                  }
+
+                  MInterface mint = (MInterface)mc;
+                  for (Iterator it3 = mint.getFeatures().iterator(); it3.hasNext(); ) {
+                      Object o = it3.next();
+                      if (o instanceof MOperation) {
+                          MOperation op = (MOperation)o;
+                          bi.addMethod(operationToMethod(op));
+                      }
+                  }
+
+
+                  //add all info types associated with mint to importedTypes
+                  for (Iterator it4=mint.getAssociationEnds().iterator();
+                       it4.hasNext();) {
+                      MClassifier mc2 = (MClassifier)((MAssociationEnd)it4.next()).getOppositeEnd().getType();
+                      if (mc2 instanceof MClass && mc2.getStereotype().getName().equals("info type")) {
+                          if (importedTypes == null) {
+                              importedTypes = new HashSet();
+                          }
+                          System.err.println("Found imported type: "+mc2);
+                          importedTypes.add(mc2);
+                      }
+                  }
+
+              }
           }
-        }
       }
-    }
-   }
+
+      System.err.println("writing imported types?");
+      //write importedTypes
+      if (importedTypes != null) {
+          System.err.println("writing imported types!");
+          TypeImport typeImport = new TypeImport();
+          is.setTypeImport(typeImport);
+          for (Iterator it2=importedTypes.iterator(); it2.hasNext();) {
+              MClass mtype = (MClass)it2.next();
+              System.err.println("writing imported type: "+mtype);
+              Type cmlType = mTypeToCMLType(mtype);
+              TypeImportItem tii = new TypeImportItem();
+              tii.setType(cmlType);
+              typeImport.addTypeImportItem(tii);
+          }
+      }
+  }
 
    private Method operationToMethod(MOperation op) {
       Method m = new Method();
@@ -206,6 +264,38 @@ public class ComponentSpecWriter
       return m;
    }
 
+
+    /*
+    private static TypeAttr mAttrToCMLAttr(MAttribute mattr) {
+        TypeAttr result = new TypeAttr();
+        result.setAttrName(mattr.getName());
+        result.setAttrTypeRef(mattr.getType().getName());
+        return result;
+    }
+    */
+
+    private static Type mTypeToCMLType(MClass mtype) {
+        Type type = new Type();
+        TypeName typeName = new TypeName();
+        typeName.setContent(mtype.getName());
+        type.setTypeName(typeName);
+        
+        TypeAttrs typeAttrs = new TypeAttrs();
+        type.setTypeAttrs(typeAttrs);
+        for (Iterator it3 = mtype.getFeatures().iterator(); it3.hasNext(); ) {
+            Object o = it3.next();
+            if (o instanceof MAttribute) {
+                MAttribute mattr = (MAttribute)o;
+
+                TypeAttr typeAttr = new TypeAttr();
+                typeAttr.setAttrName(mattr.getName());
+                typeAttr.setAttrTypeRef(mattr.getType().getName());
+
+                typeAttrs.addTypeAttr(typeAttr);
+            }
+        }
+        return type;
+    }
 
 
 }
