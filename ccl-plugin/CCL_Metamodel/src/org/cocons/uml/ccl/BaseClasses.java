4158908d3acd0310653f47aecdce59a3438bd000
fchabar <fchabar>
2002-02-07 23:12:42 +0000

    Bug fixed.

--- ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/BaseClasses.java
+++ ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/BaseClasses.java
@@ -1,10 +1,14 @@
 package org.cocons.uml.ccl;
 
-import ru.novosoft.uml.foundation.core.MElement;import ru.novosoft.uml.foundation.core.MInterface;import ru.novosoft.uml.foundation.core.MClass;/**
- * Collection of constants describing model element's base classes.
- * Creation date: (15.01.2002 16:57:24)
- * @author: Fadi Chabarek
- */
+import ru.novosoft.uml.foundation.core.MElement;
+import ru.novosoft.uml.foundation.core.MInterface;
+import ru.novosoft.uml.foundation.core.MClass;
+
+/**
+* Collection of constants describing model element's base classes.
+* Creation date: (15.01.2002 16:57:24)
+* @author: Fadi Chabarek
+*/
 public class BaseClasses {
 
 	/**
@@ -20,14 +24,14 @@ public class BaseClasses {
 	/**
 	 * Describing model elements that are business types.
 	 */
-	public static final String BUSINESS_TYPE = "BUSINESS TYPE(S)"; 
-	
+	public static final String BUSINESS_TYPE = "BUSINESS TYPE(S)";
+
 	/**
 	* Describing model elements that are components.
 	*/
-	public static final String COMPONENT = "COMPONENT(S)"; 
-	
-/**
+	public static final String COMPONENT = "COMPONENT(S)";
+
+	/**
 	* Checks weather the given object matchs the given base class type.
 	* Creation date: (07.02.2002 18:35:41)
 	* @return boolean true if the type is defined and the object matchs 
@@ -35,49 +39,50 @@ public class BaseClasses {
 	* @param possClass java.lang.Object the possible matching object.
 	* @param baseClassType java.lang.String the type to be matched
 	*/
-public boolean objectMatchsType(Object possClass, String baseClassType) {
+	public boolean objectMatchsType(Object possClass, String baseClassType) {
 
-	if(baseClassType == null || possClass == null) {
+		try {
+			if (baseClassType.compareTo(INTERFACE) == 0) {
+				return possClass instanceof MInterface;
+			}
+
+			if (baseClassType.compareTo(ELEMENT) == 0) {
+				return possClass instanceof MElement;
+			}
+
+			//TODO: Define or find constants for type, info type and comp spec...
+
+			if (baseClassType.compareTo(BUSINESS_TYPE) == 0) {
+				if (possClass instanceof MClass) {
+					return "<<type>>".compareTo(((MClass) possClass).getStereotype().getName()) == 0
+						|| "<<info type>>".compareTo(((MClass) possClass).getStereotype().getName()) == 0; 
+				}
+			}
+
+			if (baseClassType.compareTo(COMPONENT) == 0) {
+				if (possClass instanceof MClass) {
+					return "<<comp spec>>".compareTo(((MClass) possClass).getStereotype().getName()) == 0;
+				}
+			}
+		} catch (NullPointerException npe) {
+		}
 		return false;
 	}
-	
-	if (baseClassType.compareTo(INTERFACE) == 0) {
-		return possClass instanceof MInterface;
-	 }
 
-	if (baseClassType.compareTo(ELEMENT) == 0) {
-		return possClass instanceof MElement;
+	/**
+	 * Returns all defined base class types.
+	 * Creation date: (07.02.2002 19:32:04)
+	 * @return java.lang.String[] the defined base class types.
+	 */
+	public String[] getAllAvailableTypes() {
+
+		String[] types = new String[4];
+
+		types[0] = ELEMENT;
+		types[1] = INTERFACE;
+		types[2] = BUSINESS_TYPE;
+		types[3] = COMPONENT;
+
+		return types;
 	}
-
-	if(baseClassType.compareTo(BUSINESS_TYPE) == 0) {
-		if(possClass instanceof MClass) {
-			return "<<type>>".compareTo(((MClass) possClass).getStereotype().getBaseClass()) == 0 ||
-			"<<info type>>".compareTo(((MClass) possClass).getStereotype().getBaseClass()) == 0;
-		}
-	}
-
-	if(baseClassType.compareTo(COMPONENT) == 0) {
-		if(possClass instanceof MClass) {
-			return "<<comp spec>>".compareTo(((MClass) possClass).getStereotype().getBaseClass()) == 0;
-		}
-	}
-
-	return false;
 }
-
-/**
- * Returns all defined base class types.
- * Creation date: (07.02.2002 19:32:04)
- * @return java.lang.String[] the defined base class types.
- */
-public String[] getAllAvailableTypes() {
-
-	String[] types = new String[4];
-
-	types[0] = ELEMENT;
-	types[1] = INTERFACE;
-	types[2] = BUSINESS_TYPE;
-	types[3] = COMPONENT;	
-	
-	return types;
-}}
