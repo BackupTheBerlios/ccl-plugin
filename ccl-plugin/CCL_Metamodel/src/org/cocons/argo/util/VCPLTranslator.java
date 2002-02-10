e99cf4db02f1947ff167554c8f1578da6603ff1e
hyshosha <hyshosha>
2002-02-10 00:49:16 +0000

    ValidValuesDefinitionen mit Leerzeichen moeglich

--- ccl-plugin/CCL_Metamodel/src/org/cocons/argo/util/VCPLTranslator.java
+++ ccl-plugin/CCL_Metamodel/src/org/cocons/argo/util/VCPLTranslator.java
@@ -143,20 +143,27 @@ public class VCPLTranslator {
   // folgende Methoden nur für Tag-Erzeugung benutzen !!!
   public boolean checkListOfStrings(String string) {
     /* Regeln: (kann man beliebig erweitern)
-       - zulaessige Zeichen: Buchstaben, Zahlen, "-" und "_"
+       - zulaessige Zeichen: Buchstaben, Zahlen, "-" , "_" und Buchstaben
        - Strings durch Kommata getrennt
        - als erstes und letztes Zeichen des übergebenen Strings wird
          ein zulässiges Zeichen (s.o.) erwartet
-       - Leerzeichen sind unzulässig
+       - führende und abschliessende Leerzeichen eines Strings werden entfernt
+       - ein gültiger String kann nicht ausschliesslich aus Leerzeichen bestehen
        - aufeinanderfolgende Kommata sind unzulässig
     */
     // Test auf korrekten Anfang und korrektes Ende
     if (!(isValidCharacter_ListOfStrings(string.charAt(0))&&isValidCharacter_ListOfStrings(string.charAt(string.length()-1)))) return(false);
     // Test, ob nur Kommata und zulässige Zeichen
+    // und wenigstens ein Zeichen, dass kein Komma oder Leerzeichen ist
+    boolean realStringFound = false;
     for (int i = 0; i < string.length(); i++) {
       char sign = string.charAt(i);
       if (!(isValidCharacter_ListOfStrings(sign)||(sign==','))) return(false);
+      else {
+        if ((sign != ' ')&&(sign != ',')) realStringFound = true;
+      }
     }
+    if (!realStringFound) return(false);
     // Test auf aufeinanderfolgende Kommata
     for (int i = 0; i < string.length()-1; i++) {
       if (string.charAt(i)==',')
@@ -165,26 +172,86 @@ public class VCPLTranslator {
     return(true);
   }
 
+  /*
   private boolean isValidCharacter_ListOfStrings(char sign) {
     if (Character.isLetter(sign)||Character.isDigit(sign)||(sign=='-')||(sign=='_')) return(true);
     else return(false);
   }
+  */
+  private boolean isValidCharacter_ListOfStrings(char sign) {
+    if (Character.isLetter(sign)||Character.isDigit(sign)||(sign=='-')||(sign=='_')||(sign==' ')) return(true);
+    else return(false);
+  }
 
+  /*
   public String getConstraintString_ListOfStrings(String string) {
     return("\"List Of Strings\" ["+string+"]");
   }
+  */
+  public String getConstraintString_ListOfStrings(String string) {
+    // Kommata extrahieren
+    Vector strings = new Vector();
+    int beginIndex = 0;
+    int endIndex = string.length()-1;
+    int startIndex = beginIndex;
+    for (int i = startIndex; i <= endIndex; i++) {
+      if (string.charAt(i)==',') {
+        if (beginIndex == 0)  strings.addElement(string.substring(beginIndex,i));
+        else strings.addElement(string.substring(beginIndex+1,i));
+        beginIndex = i;
+      }
+      else if (i == endIndex) {
+        if (beginIndex == 0) strings.addElement(string.substring(beginIndex));
+        else strings.addElement(string.substring(beginIndex+1));
+      }
+      else {}
+    }
+    // Leerzeichen an Stringanfang und -ende entfernen
+    Vector correctStrings = new Vector();
+    for (int i = 0; i < strings.size(); i++) {
+      String correctString = deleteSpaces((String)strings.elementAt(i));
+      if (correctString != null) {
+        correctStrings.addElement(correctString);
+      }
+    }
+    // leerzeichenbereinigte Strings wieder kommagetrennt zusammensetzen
+    String formatedString = "";
+    for (int i = 0; i < correctStrings.size(); i++) {
+      formatedString = formatedString + "," + (String)correctStrings.elementAt(i);
+    }
+    // "Startkomma" entfernen
+    formatedString = formatedString.substring(1);
+    return("\"List Of Strings\" ["+formatedString+"]");
+  }
 
-  public boolean checkIntegerNumberRange(String integerRange) {
+  private String deleteSpaces(String string) {
+    int beginIndex = 0;
+    int endIndex = string.length()-1;
+    while ((beginIndex < string.length())&&(string.charAt(beginIndex)==' ')) {
+      beginIndex++;
+    }
+    while ((endIndex >= 0)&&(string.charAt(endIndex)==' ')) {
+      endIndex--;
+    }
+    String correctString = "";
+    if (endIndex == string.length()-1) correctString = string.substring(beginIndex);
+    else correctString = string.substring(beginIndex,endIndex+1);
+    if (correctString.equals("")) return(null);
+    else return(correctString);
+  }
+
+  public boolean checkIntegerNumberRange(String integerRangeDefString) {
     /* Regeln: (kann man beliebig erweitern)
        - zulaessige Zeichen: Zahlen, "-"
        - LowerValue und UpperValue durch "," getrennt
        - als erstes Zeichen wird "[" erwartet
        - als letztes Zeichen wird "]" erwartet
-       - Leerzeichen sind unzulässig
+       - Leerzeichen sind zulässig, sie werden alle entfernt
        - "[,]" steht für keine Range
        - "[,xxx]" steht für fehlende untere Grenze
        - "[xxx,]" steht für fehlende obere Grenze
     */
+    String integerRange = removeAllSpaces(integerRangeDefString);
     if (integerRange.equals("[,]")) return(true);
     if (integerRange.length()<3) return(false);
     if ((integerRange.charAt(0)!='[')||(integerRange.charAt(integerRange.length()-1)!=']')) return(false);
@@ -222,21 +289,32 @@ public class VCPLTranslator {
     return(true);
   }
 
-  public String getConstraintString_Integer(String string) {
-    return("\"Integer Number\" Range "+string);
+  private String removeAllSpaces(String string) {
+    String newString = "";
+    char sign;
+    for (int i = 0; i < string.length(); i++) {
+      sign = string.charAt(i);
+      if (sign != ' ') newString = newString + sign;
+    }
+    return(newString);
   }
 
-  public boolean checkFloatNumberRange(String floatRange) {
+  public String getConstraintString_Integer(String string) {
+    return("\"Integer Number\" Range "+removeAllSpaces(string));
+  }
+
+  public boolean checkFloatNumberRange(String floatRangeDefString) {
     /* Regeln: (kann man beliebig erweitern)
        - zulaessige Zeichen: Zahlen, "-", "."
        - LowerValue und UpperValue durch "," (Komma) getrennt
        - als erstes Zeichen wird "[" erwartet
        - als letztes Zeichen wird "]" erwartet
-       - Leerzeichen sind unzulässig
+       - Leerzeichen sind zulässig, sie werden alle entfernt
        - "[,]" steht für keine Range
        - "[,xxx]" steht für fehlende untere Grenze
        - "[xxx,]" steht für fehlende obere Grenze
     */
+    String floatRange = removeAllSpaces(floatRangeDefString);
     if (floatRange.equals("[,]")) return(true);
     if (floatRange.length()<3) return(false);
     if ((floatRange.charAt(0)!='[')||(floatRange.charAt(floatRange.length()-1)!=']')) return(false);
@@ -274,7 +352,8 @@ public class VCPLTranslator {
     return(true);
   }
 
-  public String getConstraintString_Float(String floatRange) {
+  public String getConstraintString_Float(String floatRangeDefString) {
+    String floatRange = removeAllSpaces(floatRangeDefString);
     if (floatRange.equals("[,]")) return("\"Float Number\" Range " + floatRange);
     int commaIndex = 0;
     for (int i = 1; i < floatRange.length()-1;i++) {
