02755771c9c98d7ebb95b52fde5762beb40d5c38
fchabar <fchabar>
2002-02-11 20:12:17 +0000

    Dokumentation verbesserert.

--- ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/Comparison.java
+++ ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/Comparison.java
@@ -5,37 +5,43 @@ import org.cocons.uml.ccl.context_property1_3.*;
 import ru.novosoft.uml.foundation.data_types.MMultiplicity;
 import ru.novosoft.uml.MBase;
 
-import ru.novosoft.uml.foundation.core.MModelElement;public interface Comparison {
+import ru.novosoft.uml.foundation.core.MModelElement;
+
+/**
+ * A Comparison comparing owned tagged values with model elements.
+ */
+public interface Comparison {
 
 	/**
 	 * Returns the tag (variable) from this condition.
 	 */
 	public String getTag();
 
-
-
-
-
 	/**
 	 * Returns a Comparator this comparison uses to compare values with its own.
 	 * Creation date: (26.12.2001 13:35:24)
 	 * @return org.cocons.uml.ccl.Comparison the comparison.
 	 */
-	public Comparator getComparator(); 
-	
+	public Comparator getComparator();
+
 	/**
 	* Shows if this comparison is negated.
 	* Creation date: (09.02.2002 16:00:49)
 	* @return boolean true if the comparison is negated.
 	*/
 	boolean isNegated();
+	
 	/**
-	 * Checks whether the given value lies within this comparison.
+	 * Checks whether the given model element is covered by this comparison.
 	 * Creation date: (21.12.2001 00:36:03)
-	 * @return boolean true, if the elementValue compared to the contained values is satisfactory.
-	 * @param elementValue MContextPropertyValue a tagged value contained by a ccl word.
+	 * @return boolean true, if the model element and its elementValues compared to 
+	 * the values of this comparison is satisfactory.
+	 * @param elementValue MModelElement a modelElement.
 	 */
-	boolean covers(MModelElement elementValue);	/**
-	* Returns the tagged value (constant) from this condition.
+	boolean covers(MModelElement elementValue); 
+	
+	/**
+	* Returns the tagged value(s) (constant(s)) from this comparison.
 	*/
-	public Object getValue();}
+	public Object getValue();
+}
