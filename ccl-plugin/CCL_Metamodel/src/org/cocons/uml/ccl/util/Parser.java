c9aad0c8e7f30a9ad83602da73cf3da5ab2bf785
packo <packo>
2002-02-12 13:17:58 +0000

    parser completed

--- ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/util/Parser.java
+++ ccl-plugin/CCL_Metamodel/src/org/cocons/uml/ccl/util/Parser.java
@@ -1,20 +1,29 @@
 package org.cocons.uml.ccl.util;
 
+import java.util.Vector;
+
 import org.cocons.uml.ccl.BaseClasses;
 import org.cocons.uml.ccl.Comparator;
 import org.cocons.uml.ccl.Comparison;
 import org.cocons.uml.ccl.Condition;
+import org.cocons.uml.ccl.ConditionImpl;
 import org.cocons.uml.ccl.ContextCondition;
 import org.cocons.uml.ccl.ContextConditionImpl;
 import org.cocons.uml.ccl.MContextbasedConstraint;
 import org.cocons.uml.ccl.MContextbasedConstraintImpl;
+import org.cocons.uml.ccl.SetComparison;
+import org.cocons.uml.ccl.ValueComparison;
 
+import org.cocons.uml.ccl.comparators.ComparatorFactory;
+import org.cocons.uml.ccl.comparators.ComparatorFactoryImpl;
 import org.cocons.uml.ccl.comparators.Contains;
 import org.cocons.uml.ccl.comparators.Equation;
 import org.cocons.uml.ccl.comparators.Greater;
 import org.cocons.uml.ccl.comparators.Less;
 
 import org.cocons.uml.ccl.logic_operations.And;
+import org.cocons.uml.ccl.logic_operations.LogicFactory;
+import org.cocons.uml.ccl.logic_operations.LogicFactoryImpl;
 import org.cocons.uml.ccl.logic_operations.Or;
 
 import org.cocons.uml.ccl.ccldata.CoCon;
@@ -24,6 +33,7 @@ import org.cocons.uml.ccl.ccldata.CoConSetConditionChoice;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionIntersection;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionIntersectionItem;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionQueryForSetValue;
+import org.cocons.uml.ccl.ccldata.CoConSetConditionQueryProperty;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionQuerySet;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionQuerySingleValue;
 import org.cocons.uml.ccl.ccldata.CoConSetConditionRestriction;
@@ -32,6 +42,7 @@ import org.cocons.uml.ccl.ccldata.CoConSetConditionUnionItem;
 import org.cocons.uml.ccl.ccldata.CoConSetItem;
 
 import org.cocons.uml.ccl.ccldata.types.IdType;
+import org.cocons.uml.ccl.ccldata.types.PropertyoperatorType;
 import org.cocons.uml.ccl.ccldata.types.RestrictionType;
 import org.cocons.uml.ccl.ccldata.types.SetoperatorType;
 import org.cocons.uml.ccl.ccldata.types.SingleoperatorType;
@@ -98,7 +109,29 @@ public class Parser {
 
   public static MContextbasedConstraint CoCon2MCoCon (CoCon cocon) {
     MContextbasedConstraint mcocon = new MContextbasedConstraintImpl();
-
+//set the type
+    mcocon.setCoConType(cocon.getType());
+//step through all CoConSets
+    Vector target = new Vector();
+    Vector scope = new Vector();
+    for(int i = 0; i < cocon.getCoConSet().length; i++) {
+      CoConSet set = cocon.getCoConSet(i);
+//step through all CoConSetItems of the CoConSet
+      for(int j = 0; j < set.getCoConSetItem().length; j++) {
+        CoConSetItem setItem = set.getCoConSetItem(j);
+        CoConSetCondition coconCondition = setItem.getCoConSetCondition();
+        ContextConditionImpl cc = getContextCondition(coconCondition.getCoConSetConditionChoice());
+        cc.setRange(coconCondition.getRange());
+        cc.setBaseClass(getBaseClass(coconCondition.getCoConSetConditionRestriction(0).getRestriction()));
+        if(set.getId().getType() == IdType.TARGET_TYPE) {
+          target.addElement(cc);
+        } else {
+          scope.addElement(cc);
+        }
+      }
+    }
+    mcocon.setScopeSetContextConditions(scope);
+    mcocon.setTargetSetContextConditions(target);
     return mcocon;
   }
 
@@ -132,6 +165,27 @@ public class Parser {
   }
 
   /**
+   * gets a RestrictionType and returns the equivalent BaseClass as String
+   */
+  private static String getBaseClass(RestrictionType resType) {
+    if(resType.getType() == RestrictionType.BUSINESSTYPES_TYPE)
+      return BaseClasses.BUSINESS_TYPE;
+    if(resType.getType() == RestrictionType.COMPONENTS_TYPE)
+      return BaseClasses.COMPONENT;
+    if(resType.getType() == RestrictionType.COMPUTERS_TYPE)
+      return BaseClasses.COMPUTER;
+    if(resType.getType() == RestrictionType.CONTAINERS_TYPE)
+      return BaseClasses.CONTAINER;
+    if(resType.getType() == RestrictionType.ELEMENTS_TYPE)
+      return BaseClasses.ELEMENT;
+    if(resType.getType() == RestrictionType.INFOTYPES_TYPE)
+      return BaseClasses.INFO_TYPE;
+    if(resType.getType() == RestrictionType.INTERFACES_TYPE)
+      return BaseClasses.INTERFACE;
+    return null;
+  }
+
+  /**
    * creates a ConditionChoice from a ContextCondition by steping through the whole tree
    */
   private static CoConSetConditionChoice getConditionChoice(ContextCondition cc) {
@@ -162,6 +216,41 @@ public class Parser {
       }
     }
     return choice;
+  }
+
+  /**
+   * creates a ContextCondition from the given ConditionChoice
+   */
+  private static ContextConditionImpl getContextCondition(CoConSetConditionChoice choice) {
+    ContextConditionImpl cond = new ContextConditionImpl();
+//if this choice contains only one singleValue/set/propertySet
+    if (choice.getCoConSetConditionQuerySingleValue() != null ||
+        choice.getCoConSetConditionQuerySet() != null ||
+        choice.getCoConSetConditionQueryProperty() != null) {
+      if (choice.getCoConSetConditionQuerySingleValue() != null) {
+        cond.setComparison(calculateComparison(choice.getCoConSetConditionQuerySingleValue()));
+      }
+      if (choice.getCoConSetConditionQuerySet() != null) {
+        cond.setComparison(calculateComparison(choice.getCoConSetConditionQuerySet()));
+      }
+      if (choice.getCoConSetConditionQueryProperty() != null) {
+        cond.setComparison(calculateComparison(choice.getCoConSetConditionQueryProperty()));
+      }
+    } else {
+//this choice contains a union or a intersection
+      LogicFactory lf = new LogicFactoryImpl();
+      if (choice.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.OR));
+        cond.setFirstChild(getCondition(choice.getCoConSetConditionUnion().getCoConSetConditionUnionItem(0)));
+        cond.setSecondChild(getCondition(choice.getCoConSetConditionUnion().getCoConSetConditionUnionItem(1)));
+      }
+      if (choice.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.AND));
+        cond.setFirstChild(getCondition(choice.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(0)));
+        cond.setSecondChild(getCondition(choice.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(1)));
+      }
+    }
+    return cond;
   }
 
   /**
@@ -278,6 +367,181 @@ public class Parser {
       }
     }
     return querySet;
+  }
+
+  /**
+   * creates a Comparison from a given singleValue
+   */
+  private static Comparison calculateComparison(CoConSetConditionQuerySingleValue singleValue) {
+    ValueComparison comp = new ValueComparison();
+    comp.setTag(singleValue.getProperty());
+    comp.setValue(singleValue.getValue());
+    ComparatorFactory compFac = new ComparatorFactoryImpl();
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.EQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(false);
+    }
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.NOTEQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(true);
+    }
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.GREATERTHAN_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.GREATER));
+      comp.setNegated(false);
+    }
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.GREATEREQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.LESS));
+      comp.setNegated(true);
+    }
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.LOWERTHAN_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.LESS));
+      comp.setNegated(false);
+    }
+    if(singleValue.getSingleoperator().getType() == SingleoperatorType.LOWEREQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.GREATER));
+      comp.setNegated(true);
+    }
+    return (Comparison)comp;
+  }
+
+  /**
+   * creates a Comparison from a given querySet
+   */
+  private static Comparison calculateComparison(CoConSetConditionQuerySet set) {
+    SetComparison comp = new SetComparison();
+    comp.setTag(set.getProperty());
+    comp.setValue(set.getCoConSetConditionQueryForSetValue(0).getValue());
+    ComparatorFactory compFac = new ComparatorFactoryImpl();
+    if(set.getSetoperator().getType() == SetoperatorType.CONTAINS_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.CONTAINS));
+      comp.setNegated(false);
+    }
+    if(set.getSetoperator().getType() == SetoperatorType.NOTCONTAINS_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.CONTAINS));
+      comp.setNegated(true);
+    }
+    if(set.getSetoperator().getType() == SetoperatorType.EQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(false);
+    }
+    if(set.getSetoperator().getType() == SetoperatorType.NOTEQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(true);
+    }
+    return (Comparison)comp;
+  }
+
+  /**
+   * creates a Comparison from a given queryProperty
+   */
+  private static Comparison calculateComparison(CoConSetConditionQueryProperty prop) {
+    SetComparison comp = new SetComparison();
+    comp.setTag(prop.getProperty());
+    comp.setValue(prop.getTargetproperty());
+    ComparatorFactory compFac = new ComparatorFactoryImpl();
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.CONTAINS_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.CONTAINS));
+      comp.setNegated(false);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.NOTCONTAINS_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.CONTAINS));
+      comp.setNegated(true);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.EQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(false);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.NOTEQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.EQUAL));
+      comp.setNegated(true);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.GREATERTHAN_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.GREATER));
+      comp.setNegated(false);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.GREATEREQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.LESS));
+      comp.setNegated(true);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.LOWERTHAN_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.LESS));
+      comp.setNegated(false);
+    }
+    if(prop.getPropertyoperator().getType() == PropertyoperatorType.LOWEREQUAL_TYPE) {
+      comp.setComparator(compFac.produceComparatorWithType(compFac.GREATER));
+      comp.setNegated(true);
+    }
+    return (Comparison)comp;
+  }
+
+  /**
+   * evaluates a unionItem and creates a corresponding Condition
+   */
+  private static Condition getCondition(CoConSetConditionUnionItem unionItem) {
+    Condition cond = new ConditionImpl();
+    //if this unioItem contains only one singleValue/set/propertySet
+    if (unionItem.getCoConSetConditionQuerySingleValue() != null ||
+        unionItem.getCoConSetConditionQuerySet() != null ||
+        unionItem.getCoConSetConditionQueryProperty() != null) {
+      if (unionItem.getCoConSetConditionQuerySingleValue() != null) {
+        cond.setComparison(calculateComparison(unionItem.getCoConSetConditionQuerySingleValue()));
+      }
+      if (unionItem.getCoConSetConditionQuerySet() != null) {
+        cond.setComparison(calculateComparison(unionItem.getCoConSetConditionQuerySet()));
+      }
+      if (unionItem.getCoConSetConditionQueryProperty() != null) {
+        cond.setComparison(calculateComparison(unionItem.getCoConSetConditionQueryProperty()));
+      }
+    } else {
+//this unionItem contains a union or a intersection
+      LogicFactory lf = new LogicFactoryImpl();
+      if (unionItem.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.OR));
+        cond.setFirstChild(getCondition(unionItem.getCoConSetConditionUnion().getCoConSetConditionUnionItem(0)));
+        cond.setSecondChild(getCondition(unionItem.getCoConSetConditionUnion().getCoConSetConditionUnionItem(1)));
+      }
+      if (unionItem.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.AND));
+        cond.setFirstChild(getCondition(unionItem.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(0)));
+        cond.setSecondChild(getCondition(unionItem.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(1)));
+      }
+    }
+    return cond;
+  }
+
+  /**
+   * evaluates a intersectionItem and creates a corresponding Condition
+   */
+  private static Condition getCondition(CoConSetConditionIntersectionItem itersectionItem) {
+    Condition cond = new ConditionImpl();
+//if this intersectionItem contains only one singleValue/set/propertySet
+    if (itersectionItem.getCoConSetConditionQuerySingleValue() != null ||
+        itersectionItem.getCoConSetConditionQuerySet() != null ||
+        itersectionItem.getCoConSetConditionQueryProperty() != null) {
+      if (itersectionItem.getCoConSetConditionQuerySingleValue() != null) {
+        cond.setComparison(calculateComparison(itersectionItem.getCoConSetConditionQuerySingleValue()));
+      }
+      if (itersectionItem.getCoConSetConditionQuerySet() != null) {
+        cond.setComparison(calculateComparison(itersectionItem.getCoConSetConditionQuerySet()));
+      }
+      if (itersectionItem.getCoConSetConditionQueryProperty() != null) {
+        cond.setComparison(calculateComparison(itersectionItem.getCoConSetConditionQueryProperty()));
+      }
+    } else {
+//this itersectionItem contains a union or a intersection
+      LogicFactory lf = new LogicFactoryImpl();
+      if (itersectionItem.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.OR));
+        cond.setFirstChild(getCondition(itersectionItem.getCoConSetConditionUnion().getCoConSetConditionUnionItem(0)));
+        cond.setSecondChild(getCondition(itersectionItem.getCoConSetConditionUnion().getCoConSetConditionUnionItem(1)));
+      }
+      if (itersectionItem.getCoConSetConditionUnion() != null) {
+        cond.setLogicOperation(lf.produceLogicOperationWithType(lf.AND));
+        cond.setFirstChild(getCondition(itersectionItem.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(0)));
+        cond.setSecondChild(getCondition(itersectionItem.getCoConSetConditionIntersection().getCoConSetConditionIntersectionItem(1)));
+      }
+    }
+    return cond;
   }
 
 }
